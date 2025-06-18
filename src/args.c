2e245bdd603d87229c6287e4eb8ccbe72cb8fcf4
Stephen Planck <splanck9271@gmail.com>
2025-06-18 12:21:45 -0400

    Add getopt-based argument parsing and flags

---
+++ src/args.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include "args.h"
+
+void parse_args(int argc, char *argv[], Args *args) {
+    args->use_color = 1;
+    args->show_hidden = 0;
+    args->long_format = 0;
+    args->path = ".";
+
+    static struct option long_options[] = {
+        {"no-color", no_argument, 0, 'C'},
+        {"help", no_argument, 0, 'h'},
+        {0, 0, 0, 0}
+    };
+
+    int opt;
+    while ((opt = getopt_long(argc, argv, "alCh", long_options, NULL)) != -1) {
+        switch (opt) {
+        case 'a':
+            args->show_hidden = 1;
+            break;
+        case 'l':
+            args->long_format = 1;
+            break;
+        case 'C':
+            args->use_color = 0;
+            break;
+        case 'h':
+            printf("Usage: %s [-a] [-l] [--no-color] [path]\n", argv[0]);
+            exit(0);
+            break;
+        default:
+            fprintf(stderr, "Usage: %s [-a] [-l] [--no-color] [path]\n", argv[0]);
+            exit(1);
+        }
+    }
+
+    if (optind < argc)
+        args->path = argv[optind];
+}
--- src/main.c
+++ src/main.c
@@ -1,22 +1,14 @@
 #include <stdio.h>
-#include <string.h>
 #include "list.h"
+#include "args.h"
 
 #define VLS_VERSION "0.1"
 
 int main(int argc, char *argv[]) {
-    int use_color = 1;
-    const char *path = ".";
-    for (int i = 1; i < argc; ++i) {
-        if (strcmp(argv[i], "--no-color") == 0) {
-            use_color = 0;
-        } else {
-            path = argv[i];
-        }
-    }
+    Args args;
+    parse_args(argc, argv, &args);
 
     printf("vls %s\n", VLS_VERSION);
-    list_directory(path, use_color);
+    list_directory(args.path, args.use_color, args.show_hidden, args.long_format);
     return 0;
 }
-
